281ae69cc1e1fcde515fa9f89d4eef4c7ef6eafe
Tristan Bottenberg <t_bottenberg@outlook.com>
2025-12-08 13:30:11 +0900

    Late day 7

--- 2025/Rust-Eze/src/puzzles/puzzle6/solution.rs
+++ 2025/Rust-Eze/src/puzzles/puzzle6/solution.rs
@@ -2,11 +2,90 @@ pub fn solve() {
     let file_path = "src/puzzles/puzzle6/input.txt";
 
     let contents = std::fs::read_to_string(file_path).expect("No input file");
-
     one(&contents);
     two(&contents);
 }
 
-fn one(_contents: &String) {}
+fn one(contents: &String) {
+    let numbers = transpose(
+        contents
+            .lines()
+            .take(contents.lines().count() - 1)
+            .map(|line| {
+                line.split_whitespace()
+                    .map(|c| c.parse::<u128>().unwrap())
+                    .collect()
+            })
+            .collect(),
+    );
+    let operators: Vec<char> = contents
+        .lines()
+        .last()
+        .unwrap()
+        .split_whitespace()
+        .map(|c| c.chars().collect::<Vec<char>>()[0])
+        .collect();
 
-fn two(_contents: &String) {}
+    println!(
+        "Puzzle 6.1: {}",
+        numbers
+            .iter()
+            .enumerate()
+            .map(|(n, num)| match operators[n] {
+                '+' => num.iter().copied().reduce(|x, y| x + y).unwrap(),
+                '*' => num.iter().copied().reduce(|x, y| x * y).unwrap(),
+                _ => 0_u128,
+            })
+            .sum::<u128>()
+    );
+}
+
+fn two(contents: &String) {
+    let lines = contents
+        .lines()
+        .map(|c| c.chars().collect::<Vec<char>>())
+        .collect::<Vec<Vec<char>>>();
+    let mut operator = true; // true = +, false = *
+    let mut total: u128 = 0;
+    let mut current: u128 = 0;
+    for i in 0..lines[0].len() {
+        let mut number: u128 = 0;
+        for j in 0..lines.len() - 1 {
+            if lines[j][i] != ' ' {
+                number = number * 10 + lines[j][i].to_digit(10).unwrap() as u128;
+            }
+        }
+        if number == 0 {
+            total += current;
+            current = 0;
+        } else {
+            if lines[lines.len() - 1][i] != ' ' {
+                operator = lines[lines.len() - 1][i] == '+';
+            }
+            if operator {
+                current += number;
+            } else {
+                current = if current == 0 {
+                    number
+                } else {
+                    current * number
+                };
+            }
+        }
+    }
+    total += current;
+    println!("Puzzle 6.2: {}", total);
+}
+
+fn transpose<T: Clone>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let mut result: Vec<Vec<T>> = Vec::new();
+    for _ in 0..v[0].len() {
+        result.push(Vec::<T>::new());
+    }
+    for x in 0..v.len() {
+        for y in 0..v[0].len() {
+            result[y].push(v[x][y].clone());
+        }
+    }
+    result
+}
--- 2025/Rust-Eze/src/puzzles/puzzle7/solution.rs
+++ 2025/Rust-Eze/src/puzzles/puzzle7/solution.rs
@@ -1,12 +1,94 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+
 pub fn solve() {
     let file_path = "src/puzzles/puzzle7/input.txt";
 
     let contents = std::fs::read_to_string(file_path).expect("No input file");
-
-    one(&contents);
-    two(&contents);
+    let s = Solver::new(contents);
+    s.one();
+    s.two();
 }
 
-fn one(_contents: &String) {}
+struct Solver {
+    origin: (usize, usize),
+    splitters: HashSet<(usize, usize)>,
+    end: usize,
+}
 
-fn two(_contents: &String) {}
+impl Solver {
+    fn new(contents: String) -> Self {
+        Self {
+            origin: contents
+                .lines()
+                .enumerate()
+                .flat_map(|(y, line)| {
+                    line.chars()
+                        .enumerate()
+                        .filter(|&(_, char)| char == 'S')
+                        .map(move |(x, _)| (x, y))
+                })
+                .next()
+                .expect("No 'S' found in input"),
+            splitters: contents
+                .lines()
+                .enumerate()
+                .flat_map(|(y, line)| {
+                    line.chars()
+                        .enumerate()
+                        .filter(|&(_, char)| char == '^')
+                        .map(move |(x, _)| (x, y))
+                })
+                .collect::<HashSet<(usize, usize)>>(),
+            end: contents.lines().count(),
+        }
+    }
+    fn one(&self) {
+        let mut splits = 0;
+        let mut beams: HashSet<(usize, usize)> = HashSet::new();
+        beams.insert(self.origin);
+        for _ in 0..self.end {
+            let mut new_beams: HashSet<(usize, usize)> = HashSet::new();
+            for ((x, y)) in beams {
+                if self.splitters.contains(&(x, y)) {
+                    splits += 1;
+                    new_beams.insert((x - 1, y + 1));
+                    new_beams.insert((x + 1, y + 1));
+                } else {
+                    new_beams.insert((x, y + 1));
+                }
+            }
+            beams = new_beams;
+        }
+
+        println!("Puzzle 7.1: {}", splits);
+    }
+
+    fn two(&self) {
+        let mut beams: HashMap<(usize, usize), usize> = HashMap::new();
+        beams.insert(self.origin, 1);
+        for _ in 0..self.end {
+            let mut new_beams: HashMap<(usize, usize), usize> = HashMap::new();
+            for ((x, y), n) in beams {
+                if self.splitters.contains(&(x, y)) {
+                    new_beams
+                        .entry((x - 1, y + 1))
+                        .and_modify(|m| *m += n)
+                        .or_insert(n);
+                    new_beams
+                        .entry((x + 1, y + 1))
+                        .and_modify(|m| *m += n)
+                        .or_insert(n);
+                } else {
+                    new_beams
+                        .entry((x, y + 1))
+                        .and_modify(|m| *m += n)
+                        .or_insert(n);
+                }
+            }
+            beams = new_beams;
+        }
+
+        println!("Puzzle 7.2: {}", beams.values().sum::<usize>());
+    }
+}
