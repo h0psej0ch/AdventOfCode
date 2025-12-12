8e363824e7bc0b46a13d068d17e0384ea283fbe9
Tristan Bottenberg <t_bottenberg@outlook.com>
2025-12-12 14:41:42 +0900

    Day 12: If it ain't broke, don't fix it

--- 2025/Rust-Eze/src/puzzles/puzzle12/solution.rs
+++ 2025/Rust-Eze/src/puzzles/puzzle12/solution.rs
@@ -2,11 +2,63 @@ pub fn solve() {
     let file_path = "src/puzzles/puzzle12/input.txt";
 
     let contents = std::fs::read_to_string(file_path).expect("No input file");
-
-    one(&contents);
-    two(&contents);
+    let s = Solver::new(contents);
+    s.one();
 }
 
-fn one(_contents: &String) {}
+struct Solver {
+    blocks: Vec<Vec<bool>>,
+    regions: Vec<((usize, usize), Vec<usize>)>,
+}
 
-fn two(_contents: &String) {}
+impl Solver {
+    fn new(contents: String) -> Self {
+        let splitup = contents.split("\n\n").collect::<Vec<_>>();
+        Self {
+            blocks: splitup
+                .iter()
+                .take(splitup.len() - 1)
+                .map(|block| {
+                    block
+                        .lines()
+                        .skip(1)
+                        .map(|line| line.chars().map(|c| c == '#').collect::<Vec<bool>>())
+                        .fold(Vec::new(), |mut acc, x| {
+                            acc.extend(x);
+                            acc
+                        })
+                })
+                .collect::<Vec<Vec<bool>>>(),
+            regions: splitup[splitup.len() - 1]
+                .lines()
+                .map(|line| line.split_once(": ").unwrap())
+                .map(|(size, usage)| {
+                    (
+                        size.split_once("x").unwrap(),
+                        usage
+                            .split_whitespace()
+                            .map(|x| x.parse::<usize>().unwrap())
+                            .collect::<Vec<usize>>(),
+                    )
+                })
+                .map(|((x, y), z)| ((x.parse().unwrap(), y.parse().unwrap()), z))
+                .collect::<Vec<_>>(),
+        }
+    }
+
+    fn one(&self) {
+        let block_count = self
+            .blocks
+            .iter()
+            .map(|block| block.iter().filter(|&bit| *bit).count())
+            .collect::<Vec<usize>>();
+        println!(
+            "Puzzle 12.1: {}",
+            self.regions
+                .iter()
+                .filter(|((x, y), z)| x * y
+                    >= z.iter().enumerate().map(|(i, n)| n * block_count[i]).sum())
+                .count()
+        );
+    }
+}
